653013a42d2242aba9575b6525c9c92419226da9
Halil Akca <halil.akca45@myhunter.cuny.edu>
2023-02-26 17:34:21 -0500

    created working file which outputs as intended

---
+++ queens.cpp
@@ -0,0 +1,85 @@
+/*
+    Name: Halil Akca
+    Assignment: 01 - Knights Tour
+    CSCI 235 - Professor Zamansky
+
+    This will place queens in a N*N board in such a way that no queen can take another queen in one move.
+*/
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void place(std::vector<std::string>& board, int row);
+bool validness(std::vector<std::string> board, int row, int col);
+
+std::vector<std::vector<std::string>> solutions;
+
+std::vector<std::vector<std::string>> nqueensSolve(int n) 
+{
+    std::vector<std::string> board(n,std::string(n,'.'));
+    
+    place(board, 0);
+    return solutions;
+}
+
+void place(std::vector<std::string>& board, int row)
+{
+    if(row==board.size())
+    {
+        solutions.push_back(board);
+        return;
+    }
+
+    for(int i=0; i<board.size(); i++)
+    {
+        if(validness(board, row, i))
+        {
+            board[row][i] = 'Q';
+            place(board, row+1);
+            board[row][i] = '.';
+        }
+    }
+}
+    
+bool validness(std::vector<std::string> board, int row, int col)
+{
+    for(int i=row; i>=0; i--)
+    {
+        if(board[i][col]=='Q')
+        {
+            return false;
+        }
+    }
+    
+    for(int i=row, j=col; i>=0 && j<board.size(); i--, j++)
+    {
+        if(board[i][j]=='Q')
+        {
+            return false;
+        }
+    }
+
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--)
+    {
+        if(board[i][j]=='Q')
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main()
+{
+    int size = 9;
+    nqueensSolve(size);
+
+    for (auto it = solutions[0].begin(); it != solutions[0].end(); it++)
+    {
+        std::cout << *it << "\n";
+    }
+    return 0;
+}
